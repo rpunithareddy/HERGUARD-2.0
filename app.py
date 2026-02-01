506616965b9e9e8a73686b36d28ae7239727268f
rpunithareddy <rpunithareddy06@gmail.com>
2026-02-01 10:46:48 +0530

    Refactor app to use environment variable for PORT
    Updated the app to use environment variable for port and added missing imports.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import sqlite3
+import sqlite3,os
 from datetime import datetime
 
 app = Flask(__name__)
@@ -146,4 +146,7 @@ def recent_alerts():
     return jsonify(alerts)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
